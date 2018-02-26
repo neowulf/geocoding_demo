d1df41ee63bef1a5c6e5ba542b3c81c2e2cb5f70
Siva Kommuri <snv.kommuri@gmail.com>
2018-02-26 12:41:23 -0800

    Add here and google sample json responses to reverse geocode api lookups

--- geocoding.py
+++ geocoding.py
@@ -16,6 +16,7 @@ class GeocodingContext:
     def first_address_match(self, lat, long):
         json_response = self._strategy.reverse_geocode(lat, long)
         result = self._strategy.extract_address(json_response)
+        result = self._strategy.decorate_result(result)
         return result
 
 
@@ -23,6 +24,14 @@ class GeocodingStrategy():
 
     @abc.abstractmethod
     def reverse_geocode(self, lat, long):
+        pass
+
+    @abc.abstractmethod
+    def extract_address(self, json_response):
+        pass
+
+    @abc.abstractmethod
+    def decorate_result(self, result):
         pass
 
     def invoke_api(self, url, query_dict):
@@ -54,6 +63,11 @@ class GeocodingStrategyHereApi(GeocodingStrategy):
             # TODO logging
             print('Please provide api secrets via config.ini or programmatically.')
 
+    def decorate_result(self, result):
+        return {
+            'provider': 'here',
+            'result': result
+        }
 
     def reverse_geocode(self, lat, long):
         reverse_geocode_url = "https://reverse.geocoder.cit.api.here.com/6.2/reversegeocode.json"
@@ -68,9 +82,51 @@ class GeocodingStrategyHereApi(GeocodingStrategy):
         result = []
         for json_response_view in json_response['Response']['View']:
             for json_response_result in json_response_view['Result']:
-                address = json_response_result['Location']['Address']['Label']
-                print(address)
+                # The results are in ascending order closest to the provided geo co-ords
+                if json_response_result['MatchLevel'] == 'houseNumber':
+                    address = json_response_result['Location']['Address']['Label']
+                    result.append(address)
+                    # ignoring remaining results
+                    break
+        return result
+
+
+class GeocodingStrategyGoogleApi(GeocodingStrategy):
+
+    def __init__(self, app_key=None):
+        self.app_key = app_key
+
+        try:
+            if self.app_key is None:
+                config = configparser.ConfigParser()
+                config.read('config.ini')
+                here_config = config['Google']
+                self.app_key = here_config['app_key']
+        except:
+            # TODO logging
+            print('Please provide api secrets via config.ini or programmatically.')
+
+    def decorate_result(self, result):
+        return {
+            'provider': 'google',
+            'result': result
+        }
+
+    def reverse_geocode(self, lat, long):
+        reverse_geocode_url = 'https://maps.googleapis.com/maps/api/geocode/json'
+        query_dict = {
+            'latlng': '%s,%s' % (lat, long),
+            'key': self.app_key
+        }
+
+        return self.invoke_api(reverse_geocode_url, query_dict)
+
+    def extract_address(self, json_response):
+        result = []
+        for json_response_result in json_response['results']:
+            google_location_types = json_response_result['types']
+            if 'street_address' in google_location_types:
+                address = json_response_result['formatted_address']
                 result.append(address)
-                # ignoring remaining results
                 break
         return result
--- tests.py
+++ tests.py
@@ -1,12 +1,29 @@
 import unittest
 
-from geocoding import GeocodingContext, GeocodingStrategyHereApi
+from geocoding import GeocodingContext, GeocodingStrategyHereApi, GeocodingStrategyGoogleApi
 
 
 class GeocodingTest(unittest.TestCase):
 
+    def fixture(self, context):
+        result = context.first_address_match("41.8842", "-87.6388")
+        if result['provider'] == 'google':
+            self.assertTrue(result, ['425 W Randolph St, Chicago, IL 60606, USA'])
+        elif result['provider'] == 'here':
+            self.assertTrue(result, ['425 W Randolph St, Chicago, IL 60606, United States'])
+
     def test_here_api(self):
         here_api = GeocodingStrategyHereApi()
         context = GeocodingContext(here_api)
-        result = context.first_address_match("41.8842", "-87.6388,250")
-        self.assertEqual(result, ['425 W Randolph St, Chicago, IL 60606, United States'])
+        self.fixture(context)
+
+    def test_google_api(self):
+        google_api = GeocodingStrategyGoogleApi()
+        context = GeocodingContext(google_api)
+        self.fixture(context)
+
+    def test_bad_coords(self):
+        pass
+
+    def test_fallback(self):
+        pass
